e529eb638c2b705168db34a2a20b51d769ee15a0
timoteosilvani <timoosilvani@icloud.com>
2026-07-20 15:41:03 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // forzar actualización en todos los dispositivos
 // ============================================
 
-const CACHE_VERSION = '20260720-107'; // <-- actualizar en cada deploy
+const CACHE_VERSION = '20260720-108'; // <-- actualizar en cada deploy
 const CACHE_NAME = 'fo-cache-' + CACHE_VERSION;
 
 const PRECACHE = [
